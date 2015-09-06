2b10957d72a3afac32e47f581195e094c9cc4a5b
Martijn <citybiker@live.nl>
2015-09-06 21:47:48 +0200

    No more interval checking, added crude welcome-back functionality

--- MixifyAutoWelcome/MixifyAutoWelcome.ts
+++ MixifyAutoWelcome/MixifyAutoWelcome.ts
@@ -7,48 +7,52 @@
 // @include     http://www.mixify.com/*/live/*
 // @version     1.7.2
 // @grant       none
-// @description This script can be used on Mixify.com while streaming your DJ set. The main reason why I created this script is that I couldn't see every single person who enters the stream so I thought it could be nice if script can annonce in chat who entered the stream with a warm welcome message.
+// @description This script can be used on Mixify.com while streaming your DJ set. The main reason why I created this script is that I couldn't see every single person who enters the stream so I thought it could be nice if a script can announce in chat who entered the stream with a warm welcome message.
 // ==/UserScript==
+
+/**
+ * Turn debug mode on/off
+ */
+const debugMode: boolean = true;
+
+/**
+ * A collection of welcome greetings
+ * {0} = name of the user
+ */
+const welcomeGreetings: Array<string> = ["Welcome {0}!", "Ez {0}!", "Yo {0}!", "Greetings {0}!", "Sup {0}!", "Hey {0}!", "Hi {0}!", "Whazzup {0}!", "Hello {0}!"];
+
+/**
+ * A collection of welcome back greetings
+ * {0} = name of the user
+ */
+const welcomeBackGreetings: Array<string> = ["Welcome back {0}"];
 
 class UserCollection {
     private users: Array<User> = [];
     disallowedUsers: Array<string> = [];
-    addEvents: { (user?: User) : void }[] = [];
 
     add(user: User): void {
-        console.log("Trying to add " + user.name);
-        // Check if user is allowed to be added and if it doesn't already exist
-        if (this.userIsAllowed(user.name) && !this.userExists(user.name)) {
-            this.users.push(user);
-
-            console.log("Succesfully added " + user.name);
-
-            // If the user isn't in the session yet, add the user and trigger events
-            if ($.inArray(user.name, session) === -1) {
-                console.log("Adding " + user.name + " to the session");
-                session.push(user.name);
-                for (var event of this.addEvents) {
-                    event(user);
-                }
+        logToConsole("Trying to add {0}".format(user.name));
+        if (this.userIsAllowed(user.name)) {
+            if (!this.userExists(user.id)) {
+                this.users.push(user);
+                logToConsole("Succesfully added {0} ({1})".format(user.name, user.id));
+                user.greet(welcomeGreetings);
+            } else {
+                user.greet(welcomeBackGreetings);
             }
-
-            return;
+        } else {
+            logToConsole("{0} is not allowed to be added".format(user.name));
         }
-        
-        console.log("I already know or don't want to add " + user.name);
     }
 
     private userIsAllowed(name: string): boolean {
-        if ($.inArray(name, this.disallowedUsers) !== -1) {
-            return false;
-        }
-
-        return true;
+        return $.inArray(name, this.disallowedUsers) === -1;
     }
 
-    private userExists(name: string): boolean {
+    private userExists(id: string): boolean {
         for (var user of this.users) {
-            if (user.name === name) {
+            if (user.id === id) {
                 return true;
             }
         }
@@ -58,17 +62,33 @@ class UserCollection {
 }
 
 class User {
-    constructor(name: string) {
+    constructor(id: string, name: string) {
+        this.id = id;
         this.name = name;
     }
 
+    id: string;
+
     name: string;
 
-    greet() {
-        console.log("Greeting " + this.name);
-        msg = msgList[Math.floor(Math.random() * msgList.length)];  /* Set random msg from msgList */
-        fc().fc_sendChat(msg + this.name + "!", dataString.split("=")[1]); /* Sends msg to chat */
+    greet(greetings: Array<string>): void {
+        logToConsole("Greeting {0} ({1})".format(this.name, this.id));
+        var greetingMessage = greetings[Math.floor(Math.random() * greetings.length)];
+        sendChatMessage(greetingMessage.format(this.name));
     }
+}
+
+interface String {
+    format(...replacements: string[]): string;
+}
+
+if (!String.prototype.format) {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, (match, number) => (typeof args[number] != 'undefined'
+            ? args[number]
+            : match));
+    };
 }
 
 var dj: string = $("#marqueeTitle")[0].innerHTML.replace('</h1>', '').replace('<h1>', '').trim(); /*Current DJ name*/
@@ -76,36 +96,59 @@ var me: string = $("ul#userDropDown li:eq(2) a").text().trim();  /* Your name on
 
 var users: UserCollection = new UserCollection();
 users.disallowedUsers = ["Guest", dj];
-users.addEvents.push((user) => { user.greet(); });
 
-var msgList: Array<string> = ["Welcome ", "Ez ", "Yo ", "Greetings ", "Sup ", "Hey ", "Hi ", "Whazzup ", "Hello "]; /* List of greetings */
-var msg;    /* Random generated msg for new guest */
-var session = [];  /* List of all users that entered the stream in one session */
 var url: string;
+var dataString: string;
 
 // If you are on your own stream script is running
 if (me === dj) {
     // Getting url to call AJAX
-    var dataString = $("#specatorsDockItem").attr("data-querystring");
-    url = "http://www.mixify.com/room/spectators/cache/1/?" + dataString;
+    var queryString = $("#specatorsDockItem").attr("data-querystring");
+    url = "http://www.mixify.com/room/spectators/cache/1/?" + queryString;
+    dataString = queryString.split("=")[1];
 
     if (sessionStorage.getItem("active") === null) {  /* Was this stream refreshed? */
         sessionStorage.setItem("active", "true");   /* You entered the stream for the first time */
-        session.push(dj);   /* Ignore DJ while sending welcome msg */
     } else {
-        retrieveAttenders();
+        retrieveAttendees();
     }
 
-    // Check for new guests every 5 seconds (Change 5000 ms to any other value you want)
-    setInterval(() => { retrieveAttenders(); }, 5000);
+    $('#avatarContainer').bind("DOMSubtreeModified", (e) => {
+        var element = $(e.target);
+        if (element.attr("class") === "avatar") {
+            var id = element.attr("id").split("_")[1];
+            var querystring = element.attr("data-querystring");
+            jQuery.ajaxSetup({ async: false });
+            var data = jQuery.get("http://www.mixify.com/user/info-basic/?{0}".format(querystring));
+            var responseHtml = $(data.responseText);
+            var username = $(responseHtml).find('.username')[0].innerHTML;
+            users.add(new User(id, username));
+        }
+    });
 }
 
-function retrieveAttenders() {
-    console.log("Filling attendance list");
-    jQuery.ajaxSetup({ async: false });
-    var data = jQuery.get(url);
-    var toObject = $(data.responseText);
-    $(toObject).find('.username').each((index, element) => {
-         users.add(new User(element.innerHTML.trim()));
-    });
+function retrieveAttendees(): void {
+    if (fc() != null) {
+        logToConsole("Retrieving attendance list");
+        jQuery.ajaxSetup({ async: false });
+        var data = jQuery.get(url);
+        var responseHtml = $(data.responseText);
+        $(responseHtml).find('.module-info').each((index, element) => {
+            var jqueryElement = $(element);
+            var username = jqueryElement.find('.username')[0].innerHTML;
+            var id = jqueryElement.find('.bt-wrapper').attr('data-uuid');
+            users.add(new User(id, username));
+        });
+    }
+}
+
+// Sends message to chat
+function sendChatMessage(message: string): void {
+    fc().fc_sendChat(message, dataString);
+}
+
+function logToConsole(message?: any, ...optionalParams: any[]): void {
+    if (debugMode) {
+        console.log(message, optionalParams);
+    }
 }
